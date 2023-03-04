4a5925acd5986060997979f63fe490d11484d21d
Robert R <rarDevelopment@users.noreply.github.com>
2023-03-04 15:42:48 -0500

    added basic letterboxd movies to now page

---
+++ src/app/models/letterboxd-item.spec.ts
@@ -0,0 +1,7 @@
+import { LetterboxdItem } from './letterboxd-item';
+
+describe('LetterboxdItem', () => {
+  it('should create an instance', () => {
+    expect(new LetterboxdItem()).toBeTruthy();
+  });
+});
---
+++ src/app/models/letterboxd-item.ts
@@ -0,0 +1,4 @@
+export class LetterboxdItem {
+  title: string;
+  summary: string;
+}
