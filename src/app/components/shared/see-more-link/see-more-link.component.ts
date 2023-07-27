818a6c0eb942fd8324cbbbcee8a053a063b421d2
Robert R <rarDevelopment@users.noreply.github.com>
2023-07-27 00:07:20 -0400

    added view my profile link to now page sections

---
+++ src/app/components/shared/see-more-link/see-more-link.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-see-more-link',
+  templateUrl: './see-more-link.component.html',
+  styleUrls: ['./see-more-link.component.scss']
+})
+export class SeeMoreLinkComponent {
+
+}
