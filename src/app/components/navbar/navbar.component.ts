6d283e2a80c3880546f4626facbe3d219111e692
Robert R <rarDevelopment@users.noreply.github.com>
2023-11-23 11:52:19 -0500

    add toggle for social links

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  public isSocialEnabled: boolean = environment.isSocialEnabled;
 }
