9b4f1ca985e0142ad816bb3b626a3693b497ffca
Robert R <rarDevelopment@users.noreply.github.com>
2024-01-18 02:00:06 -0500

    remove social nav button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,15 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
-    standalone: true,
-    imports: [RouterLink]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [RouterLink, NgIf],
 })
 export class HeaderComponent {
-
+  public isSocialEnabled: boolean = environment.isSocialEnabled;
 }
