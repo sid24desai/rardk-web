a00488beb3433c5c394af441af6d0411005da9ea
Robert R <rarDevelopment@users.noreply.github.com>
2024-02-01 00:31:03 -0500

    remove redundant components, change style of blog post previews

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,7 +21,6 @@ describe('AppComponent', () => {
   it(`should have as title 'rardk-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rardk-web');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rardk-web';
+  public isSocialEnabled: boolean = environment.isSocialEnabled;
+  public currentYear = new Date().getFullYear();
 }
