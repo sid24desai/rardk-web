7a5c8965bcccfced170736a449d535175ab0b799
Robert R <rarDevelopment@users.noreply.github.com>
2024-02-04 12:58:51 -0500

    split out blog component

--- src/app/components/blog/home.component.ts
+++ src/app/components/blog/home.component.ts
@@ -1,71 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { BlogService } from '../../services/blog.service';
-import { BlogPost } from 'src/app/models/blog-post';
-import { Router, RouterLink } from '@angular/router';
-import { HtmlDirective } from '../../directives/html.directive';
-import { DateDisplayComponent } from '../shared/date-display/date-display.component';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { NgIf, NgFor } from '@angular/common';
+import { Component } from '@angular/core';
 import { PageTitleComponent } from '../shared/page-title/page-title.component';
-import { BlogPostAttributes } from 'src/app/models/blog-post-attributes';
+import { BlogComponent } from './blog.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
-  imports: [
-    PageTitleComponent,
-    NgIf,
-    MatProgressSpinnerModule,
-    NgFor,
-    DateDisplayComponent,
-    HtmlDirective,
-    RouterLink,
-  ],
+  imports: [PageTitleComponent, BlogComponent],
 })
-export class HomeComponent implements OnInit {
-  public isLoading: boolean;
-  public blogPosts: BlogPost[];
-  public maxBlogPosts: number = 3;
-
-  constructor(private blogService: BlogService, private router: Router) {}
-
-  ngOnInit() {
-    this.isLoading = true;
-    this.blogService.getBlogPosts().subscribe({
-      next: (blogPosts: BlogPost[]) => {
-        console.log('returned', blogPosts);
-        console.log(blogPosts[0].attributes);
-        this.blogPosts = blogPosts
-          .sort((p1, p2) =>
-            new Date(this.getPublishedDate(p1.attributes)) >
-            new Date(this.getPublishedDate(p2.attributes))
-              ? -1
-              : 1
-          )
-          .slice(0, 3);
-        this.isLoading = false;
-      },
-      error: (error) => {
-        this.isLoading = false;
-        console.error(error);
-      },
-    });
-  }
-
-  public getPublishedDate(attributes: BlogPostAttributes): string {
-    return attributes.originallyPostedDate
-      ? attributes.originallyPostedDate
-      : attributes.publishedAt;
-  }
-
-  openBlogPost($event: MouseEvent, postId: string) {
-    // found at https://stackoverflow.com/a/64279838/386869
-    if ($event.button == 1) {
-      window.open('/blog/' + postId);
-    } else if ($event.button < 1) {
-      this.router.navigate(['blog', postId]);
-    }
-  }
-}
+export class HomeComponent {}
